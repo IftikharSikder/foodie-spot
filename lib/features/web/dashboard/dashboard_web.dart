3e20db7bfc363fa3b7d088bdb03fe1faf02ddc48
Iftikhar Ahammad Sikder <eftyshikder111@gmail.com>
2025-07-20 00:13:54 +0600

    Added app ui screenshots

--- lib/features/web/dashboard/dashboard_web.dart
+++ lib/features/web/dashboard/dashboard_web.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../address/address_widget_web.dart';
-import '../banner/screen/web_banner_carousel_widget.dart';
 
 class DashboardWeb extends StatelessWidget {
   const DashboardWeb({super.key});
@@ -13,7 +12,7 @@ class DashboardWeb extends StatelessWidget {
         child: Column(
           children: [
             WebHeaderWidget(),
-            WebBannerCarouselWidget(),
+            //WebBannerCarouselWidget(),
             //WebHorizontalCategoryWidget()
           ],
         ),
